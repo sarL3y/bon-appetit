0c0852bdbd6cb43f57d858a88c3b332d536bf57f
sarL3y <matt.tapper@gmail.com>
2019-02-27 00:06:50 -0800

    Fixed ingredients functionality / Update styling

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Hot Plate
+# Bon Appetit
 Search recipes based off a list of ingredients<br>
+See what else you'll need to complete the recipe<br>
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function formatQueryParams(params) {
 	return queryItems.join('&');
 }
 
-function getRecipes(query, limit=10) {
+function getRecipes(query, limit=5) {
 	const params = {
 		'q': query,
 		'app_id': EDAMAM_APP_ID,
@@ -44,84 +44,41 @@ function displayResults(responseJson) {
 	$('#results-list').empty();
 	console.log('emptying list');
 
-	// FOR LOOP METHOD //
 	for (let i = 0; i < responseJson.hits.length; i++) {
+		let ingredients = "";
+
 		for (let j = 0; j < responseJson.hits[i].recipe.ingredientLines.length; j++) {
-			$('#ingredients-list').append(
-			`<li>
+			ingredients = ingredients + `<li>
 				<div class="items">
 					<p class="item">${responseJson.hits[i].recipe.ingredientLines[j]}</p>
 				</div>
 			</li>`
-			);
 		};
 
 		$('#results-list').append(
 			`<li>
 				<div id="search-result" class="search-result">
-					<h3><a href="${responseJson.hits[i].recipe.url}">${responseJson.hits[i].recipe.label}</a></h3>
 					<img src="${responseJson.hits[i].recipe.image}" class="recipe-img">
-					<button type="button" id="show-ingredients" class="show-ingredients">Show Ingredients</button>
+					<a href="${responseJson.hits[i].recipe.url}" class="recipe-link">${responseJson.hits[i].recipe.label}</a>
+					<br>
+					<span class="recipe-source">${responseJson.hits[i].recipe.source}</span>
+					
+					<div class="expand-buttons">
+						<button type="button" id="show-ingredients" class="show-ingredients" data-ingredients=${i}">Ingredients</button>
+						<button type="button" id="show-health-info" class="show-health-info" data-health-info=${i}">Health Info</button>
+					</div>
 
-					<section id="ingredients" class="hidden">
-						<h4>Ingredients</h4>
+					<section id="ingredients-${i}" class="hidden">
+						<h5>Ingredients</h5>
 						<ul id="ingredients-list" class="ingredients-list">
-
+							${ingredients}
 						</ul>
 					</section>	
 				</div>
 			</li>`
 		);
-		/* for (let j = 0; j < responseJson.hits[i].recipe.ingredientLines.length; j++) {
-		$('#results-list').find('#ingredients-list').append(
-			`<li>
-				<div class="items">
-					<p class="item">${responseJson.hits[i].recipe.ingredientLines[j]}</p>
-				</div>
-			</li>`
-		);*/ 
 	};
  
-
-	// FOREACH METHOD //
-	/* responseJson.hits.forEach(key => {
-		const recipe = key.recipe;
-		const image = key.recipe.image;
-		const url = key.recipe.url;
-		const label = key.recipe.label;
-		const ingredient = key.recipe.ingredientLines.map(key => {
-			return key;
-		});
-		console.log(ingredient);
-		
-		$('#results-list').append(
-			`<li>
-				<div class="search-result">
-					<img src="${image}">
-					<h3><a href="${url}">${label}</a></h3>
-
-					<button type="button" id="show-ingredients" class="show-ingredients">Show More</button>
-
-					<section id="ingredients" class="hidden ">
-						<h4>Ingredients</h4>
-						<ul id="ingredients-list" class="ingredients-list">
-						
-						</ul>
-					</section>	
-				</div>
-			</li>`
-		);
-
-		$('#results-list').find('#ingredients-list').append(
-			`<li>
-				<div class="items">
-					<p class="item">${ingredient}</p>
-				</div>
-			</li>`
-		);		
-	}); 
-	*/
-
 	$('#results').removeClass('hidden');
 	console.log('showing stuff');
 };
@@ -137,10 +94,10 @@ function watchSearchForm() {
 
 function watchShowIngredientsButton() {
 	$('#results-list').on('click', '#show-ingredients', event => {
-		console.log('clicked + button');
-		$(this).find('#ingredients').removeClass('hidden');
+		let recipeNum = $(event.currentTarget).data('ingredients');
+		console.log(recipeNum);
+		$(this).find('#ingredients-' + recipeNum.replace('"', '')).removeClass('hidden');
 	});
-	
 }
 
 $(watchShowIngredientsButton);
